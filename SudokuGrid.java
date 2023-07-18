ab1fbfb9dcf4e87584b4d413aaccb101e406df08
Divyansh Prasad <divyanshprasad01@gmail.com>
2023-07-18 16:15:43 +0530

    Added A Sudoku game generator to fill the values in grid

--- SudokuGrid.java
+++ SudokuGrid.java
@@ -1,58 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class SudokuGrid extends JPanel {
+    cell[][] cell = new cell[9][9];
+    generateSudoko problem = new generateSudoko(9,40);
 
-    public static class texField extends JTextField implements MouseListener {
-        texField(){
-            this.setPreferredSize(new Dimension(10,10));
-            this.setBackground(new Color(0x0B2447));
-            this.setCaretColor(this.getBackground());
-            this.addMouseListener(this);
-            this.setHorizontalAlignment(JTextField.CENTER);
-            this.setForeground(new Color(0xA5D7E8));
-            this.getCaret().setVisible(false);
-            this.setFont(new Font("Helvetica",Font.BOLD,25));
-            this.setBorder(BorderFactory.createMatteBorder(1,1,1,1,new Color(0x19376D)));
-        }
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-            this.setBackground(new Color(0x576CBC));
-            this.setCaretColor(this.getBackground());
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-            this.setBackground(new Color(0x0B2447));
-            this.setCaretColor(this.getBackground());
-        }
-    }
+    int[][] values ;
 
     public class innerGrid extends JPanel{
-        innerGrid(){
+        innerGrid(int gridNum){
             this.setLayout(new GridLayout(3,3));
             this.setBackground(new Color(0x19376D));
             this.setBorder(BorderFactory.createMatteBorder(0,0,0,0,new Color(0x0B2447)));
             for(int i=0;i<9;i++){
-                this.add(new texField());
+                cell[gridNum][i] = new cell();
+                this.add(cell[gridNum][i]);
             }
         }
     }
@@ -60,8 +22,21 @@ public class SudokuGrid extends JPanel {
     SudokuGrid(){
         this.setLayout(new GridLayout(3,3));
         this.setBackground(new Color(0x19376D));
+        values = problem.getSudoko();
         for(int i=0; i<9; i++){
-            this.add(new innerGrid());
+            this.add(new innerGrid(i));
+        }
+
+        fillValues();
+
+    }
+
+    public void fillValues(){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(values[i][j]>0)
+                    cell[i][j].setText(Integer.toString(values[i][j]));
+            }
         }
     }
 }
---
+++ cell.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class cell extends JTextField implements MouseListener, ActionListener {
+
+    cell(){
+        this.setPreferredSize(new Dimension(10,10));
+        this.setBackground(new Color(0x0B2447));
+        this.setCaretColor(this.getBackground());
+        this.addMouseListener(this);
+        this.setHorizontalAlignment(JTextField.CENTER);
+        this.setForeground(new Color(0xA5D7E8));
+        this.getCaret().setVisible(false);
+        this.setFont(new Font("Helvetica",Font.BOLD,25));
+        this.setBorder(BorderFactory.createMatteBorder(1,1,1,1,new Color(0x19376D)));
+
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        this.setBackground(new Color(0x576CBC));
+        this.setCaretColor(this.getBackground());
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        this.setBackground(new Color(0x0B2447));
+        this.setCaretColor(this.getBackground());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
